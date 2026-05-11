ae62f0404c81711b6eb581988f9f9a164da19eb0
isaroryisag-design <isaroryisag@gmail.com>
2026-05-11 08:56:40 +0200

    third commit

---
+++ src/components/Move counter.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="move-counter">
+    🎯 Moves: {{ moves }}
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+// reactive state
+const moves = ref(0)
+
+// increment function (called when a tile moves)
+function increment() {
+  moves.value++
+}
+
+// reset function (called when restarting the puzzle)
+function reset() {
+  moves.value = 0
+}
+
+// expose functions so parent can call them
+defineExpose({ increment, reset })
+</script>
+
+<style scoped>
+.move-counter {
+  font-size: 18px;
+  font-weight: bold;
+  color: #444;
+  margin: 10px 0;
+}
+</style>
